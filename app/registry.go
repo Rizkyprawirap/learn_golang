30906171204eddeb4c79e50ac17b18165ec00402
SilentCat <rizkyprawirap@gmail.com>
2022-06-13 12:59:17 +0700

    first commit

---
+++ app/registry.go
@@ -0,0 +1,12 @@
+// package app
+
+// type Model struct {
+// 	Model interface{}
+// }
+
+// func RegisterModels() []Model {
+// 	return []Model{
+// 		{Model: models.User{}},
+// 		{Model: models.Address{}},
+// 	}
+// }
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/rizkyprawirap/Toko/app"
+
+func main() {
+	app.Run()
+}
